1b1ef64fcbd7793afdc68714cc6185d69c736d47
wankaiming <wanluoliang@126.com>
2024-11-25 23:57:55 +0800

    增加动态编译后，批量复制dll相关的脚本

---
+++ copydll.bat
@@ -0,0 +1,166 @@
+copy "D:\msys64\mingw64\bin\avcodec-61.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\avformat-61.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\avutil-59.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libaom.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libass-9.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libb2-1.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libbluray-2.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libbrotlicommon.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libbrotlidec.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libbrotlienc.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libbz2-1.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libcairo-2.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libcairo-gobject-2.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libcrypto-3-x64.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libdatrie-1.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libdav1d-7.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libdeflate.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libdouble-conversion.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libexpat-1.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libffi-8.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libfontconfig-1.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libfreetype-6.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libfribidi-0.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libgcc_s_seh-1.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libgdk_pixbuf-2.0-0.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libgio-2.0-0.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libglib-2.0-0.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libgme.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libgmodule-2.0-0.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libgmp-10.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libgnutls-30.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libgobject-2.0-0.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libgomp-1.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libgraphite2.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libgsm.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libharfbuzz-0.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libhogweed-6.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libhwy.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libiconv-2.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libicudt75.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libicuin75.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libicuuc75.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libidn2-0.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libintl-8.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libjasper.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libjbig-0.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libjpeg-8.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libjxl.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libjxl_cms.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libjxl_threads.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\liblc3-1.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\liblcms2-2.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libLerc.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\liblzma-5.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libmd4c.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libmng-2.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libmodplug-1.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libmp3lame-0.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libnettle-8.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libogg-0.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libopencore-amrnb-0.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libopencore-amrwb-0.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libopenjp2-7.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libopus-0.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libp11-kit-0.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libpango-1.0-0.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libpangocairo-1.0-0.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libpangoft2-1.0-0.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libpangowin32-1.0-0.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libpcre2-16-0.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libpcre2-8-0.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libpixman-1-0.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libpng16-16.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libportaudio.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\librsvg-2-2.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\librtmp-1.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libsharpyuv-0.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libsoxr.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libspeex-1.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libsrt.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libssh.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libstdc++-6.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libSvtAv1Enc-2.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libtasn1-6.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libthai-0.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libtheoradec-1.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libtheoraenc-1.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libtiff-6.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libunibreak-6.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libunistring-5.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libva.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libva_win32.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libvorbis-0.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libvorbisenc-2.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libvpl-2.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libvpx-1.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libwebp-7.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libwebpdemux-2.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libwebpmux-3.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libwinpthread-1.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libx264-164.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libx265-209.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libxml2-2.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libzstd.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\libzvbi-0.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\Qt6Core.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\Qt6Core5Compat.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\Qt6Gui.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\Qt6Network.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\Qt6Qml.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\Qt6Svg.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\Qt6Widgets.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\rav1e.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\swresample-5.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\swscale-8.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\vulkan-1.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\xvidcore.dll" C:\Users\admin\Desktop\QMPlay2
+copy "D:\msys64\mingw64\bin\zlib1.dll" C:\Users\admin\Desktop\QMPlay2
+
+mkdir C:\Users\admin\Desktop\QMPlay2\iconengines
+mkdir C:\Users\admin\Desktop\QMPlay2\imageformats
+mkdir C:\Users\admin\Desktop\QMPlay2\platforms
+mkdir C:\Users\admin\Desktop\QMPlay2\styles
+
+copy "D:\msys64\mingw64\share\qt6\plugins\iconengines\qsvgicon.dll" C:\Users\admin\Desktop\QMPlay2\iconengines
+copy "D:\msys64\mingw64\share\qt6\plugins\imageformats\qgif.dll" C:\Users\admin\Desktop\QMPlay2\imageformats
+copy "D:\msys64\mingw64\share\qt6\plugins\imageformats\qicns.dll" C:\Users\admin\Desktop\QMPlay2\imageformats
+copy "D:\msys64\mingw64\share\qt6\plugins\imageformats\qico.dll" C:\Users\admin\Desktop\QMPlay2\imageformats
+copy "D:\msys64\mingw64\share\qt6\plugins\imageformats\qjp2.dll" C:\Users\admin\Desktop\QMPlay2\imageformats
+copy "D:\msys64\mingw64\share\qt6\plugins\imageformats\qjpeg.dll" C:\Users\admin\Desktop\QMPlay2\imageformats
+copy "D:\msys64\mingw64\share\qt6\plugins\imageformats\qmng.dll" C:\Users\admin\Desktop\QMPlay2\imageformats
+copy "D:\msys64\mingw64\share\qt6\plugins\imageformats\qsvg.dll" C:\Users\admin\Desktop\QMPlay2\imageformats
+copy "D:\msys64\mingw64\share\qt6\plugins\imageformats\qtga.dll" C:\Users\admin\Desktop\QMPlay2\imageformats
+copy "D:\msys64\mingw64\share\qt6\plugins\imageformats\qtiff.dll" C:\Users\admin\Desktop\QMPlay2\imageformats
+copy "D:\msys64\mingw64\share\qt6\plugins\imageformats\qwbmp.dll" C:\Users\admin\Desktop\QMPlay2\imageformats
+copy "D:\msys64\mingw64\share\qt6\plugins\imageformats\qwebp.dll" C:\Users\admin\Desktop\QMPlay2\imageformats
+copy "D:\msys64\mingw64\share\qt6\plugins\platforms\qwindows.dll" C:\Users\admin\Desktop\QMPlay2\platforms
+copy "D:\msys64\mingw64\share\qt6\plugins\styles\qmodernwindowsstyle.dll" C:\Users\admin\Desktop\QMPlay2\styles
+
+copy "K:\QMPlay2\build\Qt_6_8_0_mingw64-Release\src\gui\QMPlay2.exe" C:\Users\admin\Desktop\QMPlay2
+
+copy "K:\QMPlay2\build\Qt_6_8_0_mingw64-Release\src\qmplay2\libqmplay2.dll" C:\Users\admin\Desktop\QMPlay2
+
+mkdir C:\Users\admin\Desktop\QMPlay2\modules\
+
+copy "K:\QMPlay2\build\Qt_6_8_0_mingw64-Release\src\modules\AudioFilters\AudioFilters.dll" C:\Users\admin\Desktop\QMPlay2\modules\
+copy "K:\QMPlay2\build\Qt_6_8_0_mingw64-Release\src\modules\Chiptune\Chiptune.dll" C:\Users\admin\Desktop\QMPlay2\modules\
+copy "K:\QMPlay2\build\Qt_6_8_0_mingw64-Release\src\modules\CUVID\CUVID.dll" C:\Users\admin\Desktop\QMPlay2\modules\
+copy "K:\QMPlay2\build\Qt_6_8_0_mingw64-Release\src\modules\Extensions\Extensions.dll" C:\Users\admin\Desktop\QMPlay2\modules\
+copy "K:\QMPlay2\build\Qt_6_8_0_mingw64-Release\src\modules\FFmpeg\FFmpeg.dll" C:\Users\admin\Desktop\QMPlay2\modules\
+copy "K:\QMPlay2\build\Qt_6_8_0_mingw64-Release\src\modules\FileAssociation\FileAssociation.dll" C:\Users\admin\Desktop\QMPlay2\modules\
+copy "K:\QMPlay2\build\Qt_6_8_0_mingw64-Release\src\modules\Inputs\Inputs.dll" C:\Users\admin\Desktop\QMPlay2\modules\
+copy "K:\QMPlay2\build\Qt_6_8_0_mingw64-Release\src\modules\Modplug\Modplug.dll" C:\Users\admin\Desktop\QMPlay2\modules\
+copy "K:\QMPlay2\build\Qt_6_8_0_mingw64-Release\src\modules\Notify\Notify.dll" C:\Users\admin\Desktop\QMPlay2\modules\
+copy "K:\QMPlay2\build\Qt_6_8_0_mingw64-Release\src\modules\Playlists\Playlists.dll" C:\Users\admin\Desktop\QMPlay2\modules\
+copy "K:\QMPlay2\build\Qt_6_8_0_mingw64-Release\src\modules\PortAudio\PortAudio.dll" C:\Users\admin\Desktop\QMPlay2\modules\
+copy "K:\QMPlay2\build\Qt_6_8_0_mingw64-Release\src\modules\QPainter\QPainter.dll" C:\Users\admin\Desktop\QMPlay2\modules\
+copy "K:\QMPlay2\build\Qt_6_8_0_mingw64-Release\src\modules\Subtitles\Subtitles.dll" C:\Users\admin\Desktop\QMPlay2\modules\
+copy "K:\QMPlay2\build\Qt_6_8_0_mingw64-Release\src\modules\VideoFilters\VideoFilters.dll" C:\Users\admin\Desktop\QMPlay2\modules\
+copy "K:\QMPlay2\build\Qt_6_8_0_mingw64-Release\src\modules\Visualizations\Visualizations.dll" C:\Users\admin\Desktop\QMPlay2\modules\
+
+mkdir C:\Users\admin\Desktop\QMPlay2\lang\
+
+for /r K:\QMPlay2\build\Qt_6_8_0_mingw64-Release\lang %%i in (*.qm) do copy "%%i" C:\Users\admin\Desktop\QMPlay2\lang\
+
+pause
